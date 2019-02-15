281088632995de0d6fab8fffd7983382270d3cf1
pilwonhur <pilwonhur@gmail.com>
2019-02-14 18:54:48 -0600

    updated kalmandecomp

--- src/MIMOControls.jl
+++ src/MIMOControls.jl
@@ -1,10 +1,6 @@
 module MIMOControls
 
-export	greet,
-		greet2
-
-greet() = print("Hello World!")
-greet2() = print("Hello World 2 !")
+export	kalmandecomp
 
 using ControlSystems, Polynomials, DifferentialEquations, LinearAlgebra
 
