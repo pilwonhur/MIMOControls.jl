0f7268d8a26e8115933459b2eac1f19503ba0e20
Pilwon Hur <pilwonhur@gmail.com>
2019-04-19 14:19:50 -0500

    added h2syn, lft, are

--- src/matrix_comps.jl
+++ src/matrix_comps.jl
@@ -755,6 +755,237 @@ function h2gram(G::StateSpace)
 	return sqrt(tr(G.B'*Wo*G.B))
 end
 
+
+
+"""`K2,Tzw=h2syn(G::StateSpace)`
+	`K2,Tzw=h2gram(G::TransferFunction)`
+Author: Pilwon Hur, Ph.D.
+
+returns h2 controller of the given system G and its LFT
+`G`: state space model of `G`
+"""
+function h2syn(G::StateSpace,ny::Int8,nu::Int8)
+	# Author: Pilwon Hur, Ph.D.
+	# 
+	# Accepts the G, state space model
+	# Accepts the G, state space model
+	# returns hinf controller of the given system G and its LFT
+	
+	A=G.A;
+	B=G.B;
+	C=G.C;
+	D=G.D;
+	nstate=size(A,1);
+	nin=size(B,2);
+	nout=size(C,1);
+	nw=nin-nu;
+	nz=nout-ny;
+
+	B1=reshape(B[:,1:nw],(nstate,nw));
+	B2=reshape(B[:,nw+1:nin],(nstate,nu));
+	C1=reshape(C[1:nz,:],(nz,nstate));
+	C2=reshape(C[nz+1:nout,:],(ny,nstate));
+	D11=reshape(D[1:nz,1:nw],(nz,nw));
+	D12=reshape(D[1:nz,nw+1:nin],(nz,nu));
+	D21=reshape(D[nz+1:nout,1:nw],(ny,nw));
+	D22=reshape(D[nz+1:nout,nw+1:nin],(ny,nu));
+
+	# controller design
+	Ruu=D12'*D12;
+	Ac=A-B2*inv(Ruu)*D12'*C1;
+	Qc=C1'*(I-D12*inv(Ruu)*D12')*C1;
+	X2=care(Ac,B2,Qc,Ruu);
+	F=-inv(Ruu)*(B2'*X2+D12'*C1);
+
+	# observer design
+	 Rww=D21*D21';
+	Ao=A-B1*D21'*inv(Rww)*C2;
+	Qo=B1*(I-D21'*inv(Rww)*D21)*B1'
+	Y2=care(Ao',C2',Qo,Rww)
+	L=-(Y2*C2'+B1*D21')*inv(Rww)
+
+	# construct the H2 controller
+	Ak=A+B2*F+L*C2;
+	Bk=-L;
+	Ck=F;
+	Dk=zeros(size(Ck,1),size(Bk,2));
+	K2=minimumreal(Ak,Bk,Ck,Dk);
+
+	Tzw=lft(G,K2);
+
+	return K2,Tzw
+end
+
+
+"""`Kinf,Tzw=hinfsyn(G::StateSpace,r,ny,nu)`
+	`Kinf,Tzw=hinfsyn(G::TransferFunction,r,ny,nu)`
+Author: Pilwon Hur, Ph.D.
+
+returns hinf controller of the given system G and its LFT
+`G`: state space model of `G`
+"""
+function hinfsyn(G::StateSpace,r,ny,nu)
+	# Author: Pilwon Hur, Ph.D.
+	# 
+	# Accepts the G, state space model
+	# returns hinf controller of the given system G and its LFT
+	
+	A=G.A;
+	B=G.B;
+	C=G.C;
+	D=G.D;
+	nstate=size(A,1);
+	nin=size(B,2);
+	nout=size(C,1);
+	nw=nin-nu;
+	nz=nout-ny;
+
+	B1=reshape(B[:,1:nw],(nstate,nw));
+	B2=reshape(B[:,nw+1:nin],(nstate,nu));
+	C1=reshape(C[1:nz,:],(nz,nstate));
+	C2=reshape(C[nz+1:nout,:],(ny,nstate));
+	D11=reshape(D[1:nz,1:nw],(nz,nw));
+	D12=reshape(D[1:nz,nw+1:nin],(nz,nu));
+	D21=reshape(D[nz+1:nout,1:nw],(ny,nw));
+	D22=reshape(D[nz+1:nout,nw+1:nin],(ny,nu));
+
+	# controller design
+	R1=D12'*D12;
+	Ac=A-B2*inv(R1)*D12'*C1;
+	Qc=C1'*(I-D12*inv(R1)*D12')'*(I-D12*inv(R1)*D12')*C1;
+	R=B1*B1'/r^2-B2*inv(R1)*B2';
+	
+	Xinf=care(Ac,I,Qc,inv(R));
+	F2=-inv(R1)*(B2'*Xinf+D12'*C1);
+	
+	# observer design
+	R2=D21*D21';
+	Ao=A-B1*D21'*inv(R2)*C2;
+	Qo=B1*(I-D21'*inv(R2)*D21)*(I-D21'*inv(R2)*D21)'*B1';
+	R=C1'*C1/r^2-C2'*inv(R2)*C2;
+	
+	Yinf=care(Ao',I,Qo,inv(R));
+	Linf=-inv(I-1/r^2*Yinf*Xinf)*(Yinf*C2'+B1*D21')*inv(R2);
+
+	F1=1/r^2 *(B1*B1'+Linf*D21*B1')*Xinf;
+
+	
+
+	# construct the Hinf controller
+	Ak=A+B2*F2+Linf*C2+F1;
+	Bk=-Linf;
+	Ck=F2;
+	Dk=zeros(size(Ck,1),size(Bk,2));
+	Kinf=minimumreal(Ak,Bk,Ck,Dk);
+
+	Tzw=lft(G,Kinf);
+
+	return Kinf,Tzw
+end
+
+
+function lft(G1::StateSpace,G2::StateSpace)
+	# if lower lft
+	G2s=minimumreal(G2);
+	nu,ny=size(G2s.D);
+	G1s=minimumreal(G1);
+	A=G1s.A;
+	B=G1s.B;
+	C=G1s.C;
+	D=G1s.D;
+
+	nstate=size(A,1);
+	nin=size(B,2);
+	nout=size(C,1);
+	nw=nin-nu;
+	nz=nout-ny;
+
+	B1=reshape(B[:,1:nw],(nstate,nw));
+	B2=reshape(B[:,nw+1:nin],(nstate,nu));
+	C1=reshape(C[1:nz,:],(nz,nstate));
+	C2=reshape(C[nz+1:nout,:],(ny,nstate));
+	D11=reshape(D[1:nz,1:nw],(nz,nw));
+	D12=reshape(D[1:nz,nw+1:nin],(nz,nu));
+	D21=reshape(D[nz+1:nout,1:nw],(ny,nw));
+	D22=reshape(D[nz+1:nout,nw+1:nin],(ny,nu));
+
+	Ak=G2s.A;
+	Bk=G2s.B;
+	Ck=G2s.C;
+	Dk=G2s.D;
+
+	E=inv(I-D22*Dk);
+	F=inv(I-Dk*D22);
+
+	TzwA=[A+B2*F*Dk*C2 B2*F*Ck;
+			Bk*E*C2 Ak+Bk*E*D22*Ck];
+	TzwB=[B1+B2*F*Dk*D21;
+			Bk*E*D21];
+	TzwC=[C1+D12*F*Dk*C2 D12*F*Ck];
+	TzwD=D11+D12*F*Dk*D21;
+
+	Tzw=minimumreal(TzwA,TzwB,TzwC,TzwD);
+	return Tzw
+end
+
+
+"""'X = ARE(A, R, Q) returns the stablizing solution (if it
+    exists) to the continuous-time Riccati equation:
+
+           A'*X + X*A - X*R*X + Q = 0
+
+    assuming R is symmetric and nonnegative definite and Q is
+    symmetric.
+"""
+function are(A,R,Q)
+	# check the size of A matrix
+	nstate,nc=size(A);
+	if nstate!=nc
+		error("Matrix A should be a square matrix.")
+	end
+
+	# check the size of Q matrix
+	n,m=size(Q);
+	if nstate!=n || nstate!=m
+		error("Dimension of Matrix Q is not correct.")
+	end
+
+	# check the size of R matrix
+	n,m=size(R);
+	if nstate!=n || nstate!=m
+		error("Dimension of Matrix R is not correct.")
+	end
+
+	# Hamiltonian matrix
+	H=[A -R;-Q -A'];
+
+	F=eigen(H);
+	tol=1e-10;
+	perm=Int64[];
+	for i=1:2*nstate
+        if abs(real(F.values[i]))<tol
+           	error("No solution: Hamiltonian matrix has eigenvalues on the imaginary axis.");
+           	break;
+       	end
+       	if real(F.values[i])<-tol
+       		push!(perm,i);
+       	end
+	end
+
+	X1=F.vectors[1:n,perm];
+	X2=F.vectors[n+1:2*n,perm];
+
+	# check if X1 is invertible
+	L,U=lu(X1);
+	if abs(prod(diag(U))) < tol
+		error("No solution: Hamiltonian matrix is not complementary.");
+	end
+
+	X=X2/X1;
+	return X;
+end
+
+
 function h2gram(G::TransferFunction)
 	return h2gram(ss(G))
 end
